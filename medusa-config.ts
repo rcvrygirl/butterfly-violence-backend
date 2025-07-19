4e6cd3e27243cd63196d8cd6b695a4154758371a
Calista Pemberton <calista@Calistas-MacBook-Air.local>
2025-07-19 21:36:48 +0800

    config fixing

--- medusa-config.ts
+++ medusa-config.ts
@@ -2,15 +2,28 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
-module.exports = defineConfig({
+// module.exports = {
+//   projectConfig: {
+//     databaseUrl: process.env.DATABASE_URL,
+//     database_type: 
+//     http: {
+//       storeCors: process.env.STORE_CORS!,
+//       adminCors: process.env.ADMIN_CORS!,
+//       authCors: process.env.AUTH_CORS!,
+//       jwtSecret: process.env.JWT_SECRET || "supersecret",
+//       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+//     }
+//   }
+// }
+
+
+module.exports = {
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    database_url: process.env.DATABASE_URL,
+    database_type: "postgres",
+    store_cors: process.env.STORE_CORS,
+    admin_cors: process.env.ADMIN_CORS,
+    jwtSecret: process.env.JWT_SECRET || "supersecret",
+    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
   }
-})
+};
